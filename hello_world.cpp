db63e64568d8d6eb597176fbe65d3d46cb268801
BuildTools <unconfigured@null.spigotmc.org>
2019-05-06 12:52:48 +0200

    added tasks

--- hello_world.cpp
+++ hello_world.cpp
@@ -17,11 +17,102 @@
 #include "Main.h"
 #include <HAL/inc/sys/alt_timestamp.h>
 #include <io.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <altera_up_avalon_parallel_port.h>
+#include "includes.h"
 
-int main()
+/* Definition of Task Stacks */
+#define   TASK_STACKSIZE       2048
+OS_STK	create_task_stk[TASK_STACKSIZE];
+OS_STK    image_task_stk[TASK_STACKSIZE];
+OS_STK	io_task_stk[TASK_STACKSIZE];
+OS_STK    robot_task_stk[TASK_STACKSIZE];
+OS_STK	button_task_stk[TASK_STACKSIZE];
+
+/* Definition of Task Priorities */
+#define create_task_priority	6
+#define image_task_priority    10
+#define io_task_priority		11
+#define robot_task_priority      12
+
+#define buf_size 16
+
+//function prototypes
+void create_task(void* pdata);
+void image_task(void* pdata);
+void io_task(void* pdata);
+void robot_task(void* pdata);
+
+OS_EVENT * coordinateQueue;
+
+void * MyBuff[buf_size];
+
+alt_up_parallel_port_dev* button = alt_up_parallel_port_open_dev("/dev/Pushbuttons");
+
+void image_task(void* pdata)
 {
+  while (1)
+  {
+    printf("Hello from image_task\n");
+    usleep(3000000);
+  }
+}
+
+void io_task(void* pdata)
+{
+	while(1){
+		printf("Hello from io_task\n");
+		usleep(100);
+	}
+}
+
+void robot_task(void* pdata)
+{
+  while (1)
+  {
+    printf("Hello from robot_task\n");
+
+    usleep(3000000);
+  }
+}
+
+
+
+void create_task(void* pdata){
+	printf("create tasks\n");
 	top.init();
-	return 0;
+//	OSTaskCreate(image_task,
+//				  NULL,
+//				  &image_task_stk[TASK_STACKSIZE-1],
+//				  image_task_priority);
+//	printf("created image task\n");
+//	OSTaskCreate(io_task,
+//				  NULL,
+//				  &io_task_stk[TASK_STACKSIZE-1],
+//				  io_task_priority);
+//
+//	printf("created io task\n");
+//	OSTaskCreate(robot_task,
+//				  NULL,
+//				  &robot_task_stk[TASK_STACKSIZE-1],
+//				  robot_task_priority);
+//	printf("created robot task\n");
+//
+//	coordinateQueue = OSQCreate(&MyBuff[0], buf_size);
+//
+	OSTaskSuspend(create_task_priority);
 
+}
 
+/* The main function creates two task and starts multi-tasking */
+int main(void)
+{
+	OSTaskCreate(create_task,
+	                  NULL,
+	                  &create_task_stk[TASK_STACKSIZE-1],
+	                  create_task_priority);
+
+  OSStart();
+  return 0;
 }
